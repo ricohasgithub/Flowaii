d0dfcfdd6f9c7b4ac3ffa85beba9fda8b397a25a
Rico Zhu <rico.zhu@duke.edu>
2022-06-14 15:32:08 -0400

    Updated atlas_model.py

---
+++ notebooks/atlas_model.py
@@ -0,0 +1,16 @@
+
+import numpy as np
+import tensorflow as tf
+
+# Update path to local installation of mlpf library
+import sys
+sys.path += ["../mlpf"]
+
+import tfmodel
+
+# Dummy function for loading atlas data; to be implemented later
+def load_data():
+    pass
+
+data = load_data()
+
